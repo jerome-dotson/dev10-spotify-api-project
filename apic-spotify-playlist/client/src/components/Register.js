84ea049c52f7e960ee39cddb92209577ecfe879f
Max Meier <funknspudzilla@MacBook-Pro.local>
2022-10-11 14:54:24 -0500

    registration stuff from login

--- apic-spotify-playlist/client/src/components/Register.js
+++ apic-spotify-playlist/client/src/components/Register.js
@@ -1,8 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { useHistory } from "react-router-dom";
+import Error from "./Error";
+import AuthContext from "../context/AuthContext";
 
 function Register() {
 
-    return(<></>);
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    const auth = useContext(AuthContext);
+
+    const history = useHistory();
+
+    return(
+        <div>
+      <h2>Register</h2>
+      {errors.map((error, i) => (
+        <Error key={i} msg={error} />
+      ))}
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            onChange={(event) => setUsername(event.target.value)}
+            id="username"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+            id="password"
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+            id="password"
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+        );
 }
 
 export default Register;
