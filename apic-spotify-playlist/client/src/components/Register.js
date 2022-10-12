6db45f5db78c28d6fbd8a0b360631791d0a37fd5
Max Meier <funknspudzilla@MacBook-Pro.local>
2022-10-12 10:35:02 -0500

    random cleanup

--- apic-spotify-playlist/client/src/components/Register.js
+++ apic-spotify-playlist/client/src/components/Register.js
@@ -13,38 +13,8 @@ function Register() {
     const [confirmPassword, setConfirmPassword] = useState("");
     const [errors, setErrors] = useState([]);
 
-    // const auth = useContext(AuthContext);
-
     const history = useHistory();
 
-    // function login() {
-
-
-
-    //     const response = fetch("http://localhost:8080/authenticate", {
-    //       method: "POST",
-    //       headers: {
-    //         "Content-Type": "application/json",
-    //       },
-    //       body: JSON.stringify({
-    //         username,
-    //         password,
-    //       }),
-    //     });
-      
-    //     if (response.status === 200) {
-    //       const { jwt_token } = response.json();
-    //       console.log(jwt_token);
-    //       auth.login(jwt_token);
-    //       history.push("/");
-    //     } else if (response.status === 403) {
-    //       setErrors(["Login failed."]);
-    //     } else {
-    //       setErrors(["Unknown error."]);
-    //     }
-    // }
-
-    //include checking if password===confirmPassword, failure prevents fetch and returns error message
     const handleSubmit = async (event) => {
         event.preventDefault();
         setErrors([]);
