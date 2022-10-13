e032f433c723e0614974ffd79dcc4e6ac0992be1
Max Meier <funknspudzilla@MacBook-Pro.local>
2022-10-13 10:23:52 -0500

    playlist controller progress

--- apic-spotify-playlist/api/src/main/java/learn/spotifyPlaylist/controllers/PlaylistController.java
+++ apic-spotify-playlist/api/src/main/java/learn/spotifyPlaylist/controllers/PlaylistController.java
@@ -1,4 +1,43 @@
 package learn.spotifyPlaylist.controllers;
 
+import learn.spotifyPlaylist.models.Playlist;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"http://localhost:3000"})
+@RequestMapping("/api/playlist")
 public class PlaylistController {
+
+    private final PlaylistService service;
+
+    public PlaylistController(PlaylistService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Playlist> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping
+    public Playlist findById(@PathVariable int playlistId){
+        return service.findById(playlistId);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Playlist playlist) {
+        Result<Playlist> result = service.add();
+        if(result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED)
+        }
+        return ErrorResponse.build(result);
+    }
+
+
+
 }
