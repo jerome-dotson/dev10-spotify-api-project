952b749839ab3966e2fa7ec8fbad6a802de3e9e6
Max Meier <funknspudzilla@MacBook-Pro.local>
2022-10-13 14:59:59 -0500

    adjustments

--- apic-spotify-playlist/client/src/components/Playlist.js
+++ apic-spotify-playlist/client/src/components/Playlist.js
@@ -1,14 +1,13 @@
-// import React from "react";
-// import { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function Playlist() {
-    // function Playlist(props) {
-    //display playlist name and username associated with app_user_id attached
-    //on click links to playlistinfo page for selected playlist
-    // return (<Link to={`/playlist/${props.playlistData.playlist_id}`}>
-    //     <h5>{props.playlistData.name}</h5>
-    //     <p>{props.playlistData.username}</p>
-    // </Link>);
+function Playlist(playlist) {
+    // display playlist name and username associated with app_user_id attached
+    // on click links to playlistinfo page for selected playlist
+    return (<Link to={`/playlist/${playlist.playlistData.playlist_id}`}>
+        <h5>{playlist.playlistData.name}</h5>
+        <p>{playlist.playlistData.username}</p>
+    </Link>);
 }
 
 export default Playlist;
