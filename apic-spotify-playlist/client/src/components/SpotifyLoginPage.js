a264a13cff01f3c50cffac3a147daf7f2e4bc111
jerome-dotson <jerome.lamar.dotson@gmail.com>
2022-10-17 23:25:40 -0400

    Added localStorage for Spotify necessities.

--- apic-spotify-playlist/client/src/components/SpotifyLoginPage.js
+++ apic-spotify-playlist/client/src/components/SpotifyLoginPage.js
@@ -3,8 +3,11 @@ import AuthContext from "../context/AuthContext";
 
 function SpotifyLoginPage(){
 
+
     const code = (new URLSearchParams(window.location.search)).get("code");
     const auth = useContext(AuthContext);
+
+
 
 
     useEffect(() => {
@@ -23,14 +26,23 @@ function SpotifyLoginPage(){
                 return response.json();
             }else{
                 console.log(response);
-                const { access_token, refresh_token, expires_in } = response; //stopped here
+                
             }
         }
-        ).then(data =>{
+        ).then(data => {
+            const {access_token, refresh_token, expires_in} = data;
+            console.log(access_token);
+            window.localStorage.setItem("current_spotify_access_token", access_token);
+            window.localStorage.setItem("current_spotify_refresh_token", refresh_token);
+            window.localStorage.setItem("current_spotify_token_time_to_expiration", expires_in);
+            window.localStorage.setItem("current_spotify_token_creation_time", String(Date.now()));
+    
+        }
 
             // {"access_token":"BQA6eFKdfNKm_HIGfsBgEhcAqgOLGV-nCWr1q9ZhXn2llDiSSeceIfALGJqBmXNa5b81l77fti3ndVAR-bQ5xLYe2nID2ekUDaszV7UKLm9pVMORXQQgeGICoTYsSuyUTMI0RreLnmcY8587FVqSSnJjH1QwHvAigEW-Kq4v3I-_n0IEpS44NKEOOrxrE21iA3UiCbOUE_RlTTDzF5HaPm_4u87V2gLhhbJ9oxyumVKdiqibsfpSJigWf3jWt_IbB4obNdy-0JhAFlCozZF0zV_IJk8S1Q","token_type":"Bearer","expires_in":3600,"refresh_token":"AQB7vIwp96yxF9qLRZdRazebz4TNjhxqGbtJV3DMiIP1zr1O_CKmCuK_fWJ1wENm4u-IFpAFJpI63EEOJ1mtZa25EeyGsSu5iy8U95C6V5lR2JftptAIorbx1x2xwo_rB8s","scope":"playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public user-read-email user-read-private"}
 
-        });
+
+        );
     }, []);
 
     return "Logged in page loading.";
